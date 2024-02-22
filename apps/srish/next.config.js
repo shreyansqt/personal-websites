8709fab2c647a7063215146fbf94d2bee95a59e5
Shreyans Jain <shreyansqt@gmail.com>
2024-02-22 22:44:22 +0100

    update next.config.js

--- apps/srish/next.config.js
+++ apps/srish/next.config.js
@@ -2,4 +2,19 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/components"],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-images-1.medium.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        port: '',
+      },
+    ],
+  },
 };
