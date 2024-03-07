db90c195e1c32e9fc021954b7df5c19652acfda7
Shreyans Jain <shreyansqt@gmail.com>
2024-03-08 00:38:28 +0100

    make portable text links open in new tab

--- packages/common/src/components/portable-text-components.tsx
+++ packages/common/src/components/portable-text-components.tsx
@@ -1,10 +1,28 @@
-import type { PortableTextComponents } from "@portabletext/react";
+import type {
+  PortableTextComponentProps,
+  PortableTextComponents,
+} from "@portabletext/react";
 import { Blockquote } from "./blockquote";
 import { ImageGallery } from "./image-gallery";
 import type { ImageGalleryProps } from "./image-gallery/image-gallery";
 
 export const portableTextComponents: PortableTextComponents = {
-  block: { blockquote: ({ children }) => <Blockquote>{children}</Blockquote> },
+  block: {
+    blockquote: ({ children }) => <Blockquote>{children}</Blockquote>,
+  },
+  marks: {
+    link: ({ value, children }) => {
+      return (
+        <a
+          href={(value as { href: string }).href}
+          rel="noopener"
+          target="_blank"
+        >
+          {children}
+        </a>
+      );
+    },
+  },
   types: {
     gallery: ({ value }: { value: ImageGalleryProps }) => {
       return (
